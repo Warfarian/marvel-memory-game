7975f98fa7120589eb65ea0418cc4f8f81f9e606
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-11-17 01:51:20 +0530

    display done for now

---
+++ src/components/DisplayImages.jsx
@@ -0,0 +1,21 @@
+
+function DisplayImages(){
+    return(
+        <div className="images"> 
+            <img src="https://thronesapi.com/assets/images/cersei.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/jon-snow.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/jaime-lannister.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/daenerys.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/arya-stark.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/tyrion-lannister.jpg" alt="" />
+            <img src="https://thronesapi.com/assets/images/varys.jpg" alt="" id="varys"/>
+            <img src="https://thronesapi.com/assets/images/the-hound.jpg" alt="" id="hound"/>
+            <img src="https://thronesapi.com/assets/images/sansa-stark.jpeg" alt="" />
+            <img src="https://thronesapi.com/assets/images/davos-seaworth.png" alt="" />
+        </div>
+    )
+}
+
+
+export {DisplayImages}
+
---
+++ src/components/ScoreCounter.jsx
@@ -0,0 +1,11 @@
+
+function ScoreCounter(){
+    return(
+        <div className="scores">
+            <div>Score: <span id="score"></span></div>
+            <div>High Score: <span id="highScore"></span></div>
+        </div>
+    )
+}
+
+export {ScoreCounter}
